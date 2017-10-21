957ad7ebbdcaa6971575eea73298bdeec4261676
Ramon Romero y Vigil <github@romeroyvigil.com>
2017-10-21 09:35:17 -0400

    Initial checkin of package file.

---
+++ src/main/scala/io/functionalzen/functrix/package.scala
@@ -0,0 +1,12 @@
+package io.functionalzen
+
+/**
+  * Functrix is a functional replacement to the hystrix library.
+  *
+  * Created by ramonromeroyvigil on 10/21/17.
+  */
+package object functrix {
+
+}//end package object functrix
+
+//31337
