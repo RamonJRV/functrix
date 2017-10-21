7226adb6ea9c824fe3f731b8abca86c658b5d361
RamonJRV <github@romeroyvigil.com>
2017-10-21 12:02:40 -0400

    Initial implementation of Func.

--- src/main/scala/io/functionalzen/functrix/Func.scala
+++ src/main/scala/io/functionalzen/functrix/Func.scala
@@ -1,9 +1,29 @@
 package io.functionalzen.functrix
 
+import scala.concurrent.Future
+
+object Func {
+
+  type FuncOutput[O] = Future[O]
+
+}//end object Func
+
 /**
   * Created by ramonromeroyvigil on 10/21/17.
   */
-trait Func {
+trait Func[I,O] { self =>
+
+  import Func.FuncOutput
+
+
+  def apply(input : I) : FuncOutput[O]
+
+  final def map[P](f : (I => FuncOutput[O]) => I => FuncOutput[P]): Func[I, P] = new Func[I, P] {
+    override def apply(input: I) : FuncOutput[P] = f(self.apply)(input)
+  }
+
+  final def flatMap[P](f : (I => FuncOutput[O]) => Func[I,P]) : Func[I,P] = f(self.apply)
+
 
 }//end trait Func
 
